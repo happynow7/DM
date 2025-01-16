b7dc70178e8c863960ebbdd45386afdfc80e9088
hyein <helen6662@naver.com>
2025-01-16 12:52:23 +0900

    [refactor] 회원가입 View 로직 수정

--- django-react/frontend/src/components/UserPage/SignupPage.js
+++ django-react/frontend/src/components/UserPage/SignupPage.js
@@ -4,77 +4,75 @@ import { Input } from 'antd';
 import styled from 'styled-components';
 
 const SignupDiv = styled.div`
-  padding: 3rem;
+    padding: 3rem;
     form {
-    width: 320px;
-    display: inline-block;
-    label {
-      margin-bottom: 1rem;
+        width: 320px;
+        display: inline-block;
+        label {
+            margin-bottom: 1rem;
+        }
+        input {
+            margin-bottom: 1.5rem;
+            &[type=submit] {
+                background: black;
+                color: white;
+                margin-top: 1rem;
+            }
+        }
     }
-    input {
-      margin-bottom: 1.5rem;
-      &[type=submit] {
-        background: black;
-        color: white;
-        margin-top: 1rem;
-      }
-    }
-  }
 `;
 
 const SignupPage = () => {
-    const [email, setEmail] = useState('')
-    const [password1, setPassword1] = useState('')
-    const [password2, setPassword2] = useState('')
-    const [errors, setErrors] = useState(false)
+    const [username, setUsername] = useState(''); // 사용자명 추가
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [errors, setErrors] = useState(false);
 
-    const onChangeEmail = (e) => {
-        setEmail(e.target.value)
-    }
+    const onChangeUsername = (e) => {
+        setUsername(e.target.value); // 사용자명 업데이트
+    };
 
     const onChangePwd1 = (e) => {
-        setPassword1(e.target.value)
-    }
+        setPassword1(e.target.value);
+    };
 
     const onChangePwd2 = (e) => {
-        setPassword2(e.target.value)
-    }
+        setPassword2(e.target.value);
+    };
 
     const onSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
         const user = {
-            email: email,
-            password1: password1,
-            password2: password2
-        }
+            username: username, // 사용자명으로 변경
+            password: password1, // 비밀번호로 변경
+        };
 
         // 유효성 검사
-        if(password1 !== password2) {
-            alert('비밀번호와 비밀번호 확인이 일치하지 않습니다')
-            return false
+        if (password1 !== password2) {
+            alert('비밀번호와 비밀번호 확인이 일치하지 않습니다');
+            return false;
         }
 
         Axios.post('/api/v1/mall/auth/register/', user)
             .then(res => {
-                if (res.data.key) {
-                    localStorage.clear()
-                    localStorage.setItem('token', res.data.key)
+                if (res.data.success) { // 성공 메시지에 맞게 수정
+                    localStorage.clear();
                     // 사용하려면 App.js에서 /로 라우팅해야 한다
-                    window.location.replace('/')
+                    window.location.replace('/');
                 } else {
-                    setEmail('')
-                    setPassword1('')
-                    setPassword2('')
-                    localStorage.clear()
-                    setErrors(true)
+                    setUsername('');
+                    setPassword1('');
+                    setPassword2('');
+                    localStorage.clear();
+                    setErrors(true);
                 }
             })
             .catch(err => {
-                console.clear()
-                alert('아이디 혹은 비밀번호가 일치하지 않습니다')
-            })
-    }
+                console.clear();
+                alert('아이디 혹은 비밀번호가 일치하지 않습니다');
+            });
+    };
 
     return (
         <SignupDiv>
@@ -82,11 +80,11 @@ const SignupPage = () => {
             <br />
             {errors === true && <h2>Cannot signup with provided credentials</h2>}
             <form onSubmit={onSubmit}>
-                <label htmlFor='email'>이메일 주소:</label>
+                <label htmlFor='username'>사용자명:</label>
                 <Input
-                    type='email'
-                    value={email}
-                    onChange={onChangeEmail}
+                    type='text'
+                    value={username}
+                    onChange={onChangeUsername}
                     required
                 />
                 <label htmlFor='password1'>비밀번호(소문자, 숫자, 특수문자 포함 8~16자):</label>
@@ -111,7 +109,7 @@ const SignupPage = () => {
                 <Input type='submit' size="large" value='가입하기' />
             </form>
         </SignupDiv>
-    )
-}
+    );
+};
 
 export default SignupPage;
